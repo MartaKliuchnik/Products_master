255740eadd745f6760cdf1d7ee1f56d27bcad4fb
Marta <martasagun95@gmail.com>
2022-10-19 15:54:35 +0200

    added empty basket

--- src/components/BasketContainer/index.jsx
+++ src/components/BasketContainer/index.jsx
@@ -3,16 +3,22 @@ import { useContext } from 'react';
 import { Context } from '../../context';
 import BasketItem from '../BasketItem';
 import s from './index.module.sass';
+import EmptyBasket from '../EmptyBasket'
 
 export default function BasketContainer() {
 
     const { orders } = useContext(Context);
 
     return (
-        <div className={['wrapper', s.basket_container].join(' ')}>
+        <>
             {
-                orders.map(order => <BasketItem key={order.id} {...order} />)
+                orders.length === 0
+                    ? <EmptyBasket />
+                :
+                <div className={['wrapper', s.basket_container].join(' ')}>
+                        {orders.map(order => <BasketItem key={order.id} {...order} />)}
+                </div>
             }
-        </div>
+        </>
     )
 }
